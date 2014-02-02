feaea8564fba6571b749d3b2d33838d1aacb7ccc
Oliver Klee <typo3-coding@oliverklee.de>
2014-02-02 19:36:02 +0100

    [CLEANUP] Fix warnings about spaces around arithmetic operators.

--- emogrifier.php
+++ emogrifier.php
@@ -339,7 +339,12 @@ class Emogrifier {
 
         // We want these sorted in ascending order so selectors with lesser precedence get processed first and
         // selectors with greater precedence get sorted last.
-        return ($precedenceA == $precedenceB) ? ($a['line'] < $b['line'] ? -1 : 1) : ($precedenceA < $precedenceB ? -1 : 1);
+        // The parenthesis around the -1 are necessary to avoid a PHP_CodeSniffer warning about missing spaces around
+        // arithmetic operators.
+        // @see http://forge.typo3.org/issues/55605
+        $precedenceForEquals = ($a['line'] < $b['line'] ? (-1) : 1);
+        $precedenceForNotEquals = ($precedenceA < $precedenceB ? (-1) : 1);
+        return ($precedenceA === $precedenceB) ? $precedenceForEquals : $precedenceForNotEquals;
     }
 
     /**
