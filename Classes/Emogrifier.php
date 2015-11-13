36e34b30bc71f5ff3d14b3c178e399ef49ba4495
Peter Fox <peter.fox@myjobmatcher.com>
2015-11-13 14:11:08 +0000

    Adds a mechanism for adding css to a style block as well as it being inlined

--- Classes/Emogrifier.php
+++ Classes/Emogrifier.php
@@ -154,6 +154,13 @@ class Emogrifier
     private $shouldKeepInvisibleNodes = true;
 
     /**
+     * Determines whether the css should be added as a style element as a back up for some email providers
+     *
+     * @var bool
+     */
+    private $shouldAppendCssAsStyleNode = true;
+
+    /**
      * The constructor.
      *
      * @param string $html the HTML to emogrify, must be UTF-8-encoded
@@ -327,6 +334,9 @@ class Emogrifier
         }
 
         $this->copyCssWithMediaToStyleNode($xmlDocument, $xPath, $cssParts['media']);
+        if($this->shouldAppendCssAsStyleNode) {
+            $this->addStyleElementToDocument($xmlDocument, $cssParts['css']);
+        }
     }
 
     /**
@@ -378,6 +388,16 @@ class Emogrifier
         }
 
         return $this->caches[self::CACHE_KEY_CSS][$cssKey];
+    }
+
+    /**
+     * Disables duplicating the CSS into a style block after in-lining
+     *
+     * @return void
+     */
+    public function disableBackupCssNode()
+    {
+        $this->shouldAppendCssAsStyleNode = false;
     }
 
     /**
