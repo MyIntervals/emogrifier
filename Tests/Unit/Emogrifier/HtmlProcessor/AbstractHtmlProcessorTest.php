81bc750d6e6c86e5d0338718a1d47178cb8ff5ca
Oliver Klee <github@oliverklee.de>
2018-01-13 20:34:20 +0100

    [FEATURE] Add an AbstractHtmlProcessor class (#486)
    This class will be the base class for other classes that can remove
    invisible nodes, convert CSS to non-CSS attributes etc.
    
    The folder structure will be cleaned up once the Emogrifier class has
    been renamed to CssInliner and moved from the Pelago\ namespace to the
    Pelage\Emogrifier namespace (which will happen in Emogrifier 4.0.0).

---
+++ Tests/Unit/Emogrifier/HtmlProcessor/AbstractHtmlProcessorTest.php
@@ -0,0 +1,321 @@
+<?php
+
+namespace Pelago\Tests\Unit\Emogrifier\HtmlProcessor;
+
+use Pelago\Emogrifier\HtmlProcessor\AbstractHtmlProcessor;
+use Pelago\Tests\Unit\Emogrifier\HtmlProcessor\Fixtures\TestingHtmlProcessor;
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <github@oliverklee.de>
+ */
+class AbstractHtmlProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function fixtureIsAbstractHtmlProcessor()
+    {
+        static::assertInstanceOf(AbstractHtmlProcessor::class, new TestingHtmlProcessor('<html></html>'));
+    }
+
+    /**
+     * @test
+     */
+    public function reformatsHtml()
+    {
+        $rawHtml = '<!DOCTYPE HTML>' .
+            '<html>' .
+            '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>' .
+            '<body></body>' .
+            '</html>';
+        $formattedHtml = '<!DOCTYPE HTML>' . PHP_EOL .
+            '<html>' . PHP_EOL .
+            '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>' . PHP_EOL .
+            '<body></body>' . PHP_EOL .
+            '</html>' . PHP_EOL;
+
+        $subject = new TestingHtmlProcessor($rawHtml);
+
+        static::assertSame($formattedHtml, $subject->render());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function nonHtmlDataProvider()
+    {
+        return [
+            'empty string' => [''],
+            'null' => [null],
+            'integer' => [2],
+            'float' => [3.14159],
+            'object' => [new \stdClass()],
+        ];
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     *
+     * @param mixed $html
+     * @dataProvider nonHtmlDataProvider
+     */
+    public function constructorWithNoHtmlDataThrowsException($html)
+    {
+        new TestingHtmlProcessor($html);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function invalidHtmlDataProvider()
+    {
+        return [
+            'broken nesting gets nested' => ['<b><i></b></i>', '<b><i></i></b>'],
+            'partial opening tag gets closed' => ['<b', '<b></b>'],
+            'only opening tag gets closed' => ['<b>', '<b></b>'],
+            'only closing tag gets removed' => ['foo</b> bar', 'foo bar'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param string $input
+     * @param string $expectedHtml
+     * @dataProvider invalidHtmlDataProvider
+     */
+    public function renderRepairsBrokenHtml($input, $expectedHtml)
+    {
+        $subject = new TestingHtmlProcessor($input);
+        $result = $subject->render();
+
+        static::assertContains($expectedHtml, $result);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function contentWithoutHtmlTagDataProvider()
+    {
+        return [
+            'doctype only' => ['<!DOCTYPE html>'],
+            'body content only' => ['<p>Hello</p>'],
+            'HEAD element' => ['<head></head>'],
+            'BODY element' => ['<body></body>'],
+            'HEAD AND BODY element' => ['<head></head><body></body>'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $html
+     * @dataProvider contentWithoutHtmlTagDataProvider
+     */
+    public function addsMissingHtmlTag($html)
+    {
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        static::assertContains('<html>', $result);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function contentWithoutHeadTagDataProvider()
+    {
+        return [
+            'doctype only' => ['<!DOCTYPE html>'],
+            'body content only' => ['<p>Hello</p>'],
+            'BODY element' => ['<body></body>'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $html
+     * @dataProvider contentWithoutHeadTagDataProvider
+     */
+    public function addsMissingHeadTag($html)
+    {
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        static::assertContains('<head>', $result);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function contentWithoutBodyTagDataProvider()
+    {
+        return [
+            'doctype only' => ['<!DOCTYPE html>'],
+            'HEAD element' => ['<head></head>'],
+            'body content only' => ['<p>Hello</p>'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $html
+     * @dataProvider contentWithoutBodyTagDataProvider
+     */
+    public function addsMissingBodyTag($html)
+    {
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        static::assertContains('<body>', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function putsMissingBodyElementAroundBodyContent()
+    {
+        $subject = new TestingHtmlProcessor('<p>Hello</p>');
+
+        $result = $subject->render();
+
+        static::assertContains('<body><p>Hello</p></body>', $result);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function specialCharactersDataProvider()
+    {
+        return [
+            'template markers with dollar signs & square brackets' => ['$[USER:NAME]$'],
+            'UTF-8 umlauts' => ['Küss die Hand, schöne Frau.'],
+            'HTML entities' => ['a &amp; b &gt; c'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $codeNotToBeChanged
+     * @dataProvider specialCharactersDataProvider
+     */
+    public function keepsSpecialCharacters($codeNotToBeChanged)
+    {
+        $html = '<html><p>' . $codeNotToBeChanged . '</p></html>';
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        static::assertContains($codeNotToBeChanged, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function addMissingHtml5DocumentType()
+    {
+        $subject = new TestingHtmlProcessor('<html></html>');
+
+        $result = $subject->render();
+
+        static::assertContains('<!DOCTYPE html>', $result);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function documentTypeDataProvider()
+    {
+        return [
+            'HTML5' => ['<!DOCTYPE html>'],
+            'XHTML 1.0 strict' => [
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' .
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
+            ],
+            'XHTML 1.0 transitional' => [
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ' .
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+            ],
+            'HTML 4 transitional' => [
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" ' .
+                '"http://www.w3.org/TR/REC-html40/loose.dtd">'
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $documentType
+     * @dataProvider documentTypeDataProvider
+     */
+    public function keepsExistingDocumentType($documentType)
+    {
+        $html = $documentType . '<html></html>';
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        static::assertContains($documentType, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function addsMissingContentTypeMetaTag()
+    {
+        $subject = new TestingHtmlProcessor('<p>Hello</p>');
+
+        $result = $subject->render();
+
+        static::assertContains('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function notAddsSecondContentTypeMetaTag()
+    {
+        $html = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>';
+        $subject = new TestingHtmlProcessor($html);
+
+        $result = $subject->render();
+
+        $numberOfContentTypeMetaTags = substr_count($result, 'Content-Type');
+        static::assertSame(1, $numberOfContentTypeMetaTags);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $documentType
+     * @dataProvider documentTypeDataProvider
+     */
+    public function convertsXmlSelfClosingTagsToNonXmlSelfClosingTag($documentType)
+    {
+        $subject = new TestingHtmlProcessor($documentType . '<html><body><br/></body></html>');
+
+        $result = $subject->render();
+
+        static::assertContains('<body><br></body>', $result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $documentType
+     * @dataProvider documentTypeDataProvider
+     */
+    public function keepsNonMmlSelfClosingTags($documentType)
+    {
+        $subject = new TestingHtmlProcessor($documentType . '<html><body><br></body></html>');
+
+        $result = $subject->render();
+
+        static::assertContains('<body><br></body>', $result);
+    }
+}
