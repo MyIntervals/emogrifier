088991b774bdb32f45c39d68ed2a33496ccad08f
Oliver Klee <github@oliverklee.de>
2018-02-20 23:40:00 +0100

    [CLEANUP] Autoformat the code

--- tests/Unit/EmogrifierTest.php
+++ tests/Unit/EmogrifierTest.php
@@ -1413,9 +1413,9 @@ class EmogrifierTest extends \PHPUnit_Framework_TestCase
             'allowed but non-matching media rule' => '@media screen { h6 { color: #f00; } }',
         ];
         $possibleCssBefore = $possibleSurroundingCss + [
-            '@import' => '@import "foo.css";',
-            '@charset' => '@charset "UTF-8";',
-        ];
+                '@import' => '@import "foo.css";',
+                '@charset' => '@charset "UTF-8";',
+            ];
 
         $datasets = [];
         foreach ($possibleCssBefore as $descriptionBefore => $cssBefore) {
