c3c5b9d02d57f3eeb383f1243d8312d9129c1a2b
Jake Hotson <jake@qzdesign.co.uk>
2018-03-11 23:33:35 +0000

    [BUGFIX] Merge #534 into CssInliner

--- src/Emogrifier/CssInliner.php
+++ src/Emogrifier/CssInliner.php
@@ -1064,8 +1064,11 @@ class CssInliner
             $mediaTypesExpression = '|' . implode('|', array_keys($this->allowedMediaTypes));
         }
 
+        $mediaRuleBodyMatcher = '[^{]*+{(?:[^{}]*+{.*})?\\s*+}\\s*+';
+
         $cssSplitForAllowedMediaTypes = preg_split(
-            '#(@media\\s++(?:only\\s++)?+(?:[\\s{\\(]\\s*' . $mediaTypesExpression . ')\\s*[^{]*+{.*}\\s*}\\s*)#misU',
+            '#(@media\\s++(?:only\\s++)?+(?:(?=[{\\(])' . $mediaTypesExpression . ')' . $mediaRuleBodyMatcher
+                . ')#misU',
             $cssWithoutComments,
             -1,
             PREG_SPLIT_DELIM_CAPTURE
@@ -1074,7 +1077,7 @@ class CssInliner
         // filter the CSS outside/between allowed @media rules
         $cssCleaningMatchers = [
             'import/charset directives' => '/\\s*+@(?:import|charset)\\s[^;]++;/i',
-            'remaining media enclosures' => '/\\s*+@media\\s[^{]+{(.*)}\\s*}\\s/isU',
+            'remaining media enclosures' => '/\\s*+@media\\s' . $mediaRuleBodyMatcher . '/isU',
         ];
 
         $splitCss = [];
