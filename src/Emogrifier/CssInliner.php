769cfa03118187ff3ca19c3afa8add14c64d70df
Oliver Klee <github@oliverklee.de>
2019-05-24 17:21:04 +0200

    [TASK] Make the CssInliner inherit AbstractHtmlProcessor (#660)
    Closes #554

--- src/Emogrifier/CssInliner.php
+++ src/Emogrifier/CssInliner.php
@@ -2,6 +2,7 @@
 
 namespace Pelago\Emogrifier;
 
+use Pelago\Emogrifier\HtmlProcessor\AbstractHtmlProcessor;
 use Symfony\Component\CssSelector\CssSelectorConverter;
 use Symfony\Component\CssSelector\Exception\SyntaxErrorException;
 
@@ -21,7 +22,7 @@ use Symfony\Component\CssSelector\Exception\SyntaxErrorException;
  * @author Sander Kruger <s.kruger@invessel.com>
  * @author Zoli Szabó <zoli.szabo+github@gmail.com>
  */
-class CssInliner
+class CssInliner extends AbstractHtmlProcessor
 {
     /**
      * @var int
@@ -52,30 +53,6 @@ class CssInliner
      * @var string
      */
     const PSEUDO_CLASS_MATCHER = '\\S+\\-(?:child|type\\()|not\\([[:ascii:]]*\\)';
-
-    /**
-     * @var string
-     */
-    const CONTENT_TYPE_META_TAG = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
-
-    /**
-     * @var string
-     */
-    const DEFAULT_DOCUMENT_TYPE = '<!DOCTYPE html>';
-
-    /**
-     * @var string Regular expression part to match tag names that PHP's DOMDocument implementation is not aware are
-     *      self-closing. These are mostly HTML5 elements, but for completeness <command> (obsolete) and <keygen>
-     *      (deprecated) are also included.
-     *
-     * @see https://bugs.php.net/bug.php?id=73175
-     */
-    const PHP_UNRECOGNIZED_VOID_TAGNAME_MATCHER = '(?:command|embed|keygen|source|track|wbr)';
-
-    /**
-     * @var \DOMDocument
-     */
-    protected $domDocument = null;
 
     /**
      * @var bool[]
@@ -174,93 +151,15 @@ class CssInliner
     private $debug = false;
 
     /**
-     * @param string $unprocessedHtml raw HTML, must be UTF-encoded, must not be empty
-     *
-     * @throws \InvalidArgumentException if $unprocessedHtml is anything other than a non-empty string
+     * @return CssSelectorConverter
      */
-    public function __construct($unprocessedHtml)
+    private function getCssSelectorConverter()
     {
-        if (!\is_string($unprocessedHtml)) {
-            throw new \InvalidArgumentException('The provided HTML must be a string.', 1540403176);
-        }
-        if ($unprocessedHtml === '') {
-            throw new \InvalidArgumentException('The provided HTML must not be empty.', 1540403181);
+        if ($this->cssSelectorConverter === null) {
+            $this->cssSelectorConverter = new CssSelectorConverter();
         }
 
-        $this->cssSelectorConverter = new CssSelectorConverter();
-
-        $this->setHtml($unprocessedHtml);
-    }
-
-    /**
-     * Sets the HTML to process.
-     *
-     * @param string $html the HTML to process, must be UTF-8-encoded
-     *
-     * @return void
-     */
-    private function setHtml($html)
-    {
-        $this->createUnifiedDomDocument($html);
-    }
-
-    /**
-     * Provides access to the internal DOMDocument representation of the HTML in its current state.
-     *
-     * @return \DOMDocument
-     */
-    public function getDomDocument()
-    {
-        return $this->domDocument;
-    }
-
-    /**
-     * Renders the normalized and processed HTML.
-     *
-     * @return string
-     */
-    public function render()
-    {
-        $htmlWithPossibleErroneousClosingTags = $this->domDocument->saveHTML();
-
-        return $this->removeSelfClosingTagsClosingTags($htmlWithPossibleErroneousClosingTags);
-    }
-
-    /**
-     * Renders the content of the BODY element of the normalized and processed HTML.
-     *
-     * @return string
-     */
-    public function renderBodyContent()
-    {
-        $htmlWithPossibleErroneousClosingTags = $this->domDocument->saveHTML($this->getBodyElement());
-        $bodyNodeHtml = $this->removeSelfClosingTagsClosingTags($htmlWithPossibleErroneousClosingTags);
-
-        return \str_replace(['<body>', '</body>'], '', $bodyNodeHtml);
-    }
-
-    /**
-     * Eliminates any invalid closing tags for void elements from the given HTML.
-     *
-     * @param string $html
-     *
-     * @return string
-     */
-    private function removeSelfClosingTagsClosingTags($html)
-    {
-        return \preg_replace('%</' . static::PHP_UNRECOGNIZED_VOID_TAGNAME_MATCHER . '>%', '', $html);
-    }
-
-    /**
-     * Returns the BODY element.
-     *
-     * This method assumes that there always is a BODY element.
-     *
-     * @return \DOMElement
-     */
-    private function getBodyElement()
-    {
-        return $this->domDocument->getElementsByTagName('body')->item(0);
+        return $this->cssSelectorConverter;
     }
 
     /**
@@ -273,57 +172,6 @@ class CssInliner
     private function getHeadElement()
     {
         return $this->domDocument->getElementsByTagName('head')->item(0);
-    }
-
-    /**
-     * Creates a DOM document from the given HTML and stores it in $this->domDocument.
-     *
-     * The DOM document will always have a BODY element and a document type.
-     *
-     * @param string $html
-     *
-     * @return void
-     */
-    private function createUnifiedDomDocument($html)
-    {
-        $this->createRawDomDocument($html);
-        $this->ensureExistenceOfBodyElement();
-    }
-
-    /**
-     * Creates a DOMDocument instance from the given HTML and stores it in $this->domDocument.
-     *
-     * @param string $html
-     *
-     * @return void
-     */
-    private function createRawDomDocument($html)
-    {
-        $domDocument = new \DOMDocument();
-        $domDocument->strictErrorChecking = false;
-        $domDocument->formatOutput = true;
-        $libXmlState = \libxml_use_internal_errors(true);
-        $domDocument->loadHTML($this->prepareHtmlForDomConversion($html));
-        \libxml_clear_errors();
-        \libxml_use_internal_errors($libXmlState);
-
-        $this->domDocument = $domDocument;
-    }
-
-    /**
-     * Returns the HTML with added document type, Content-Type meta tag, and self-closing slashes, if needed,
-     * ensuring that the HTML will be good for creating a DOM document from it.
-     *
-     * @param string $html
-     *
-     * @return string the unified HTML
-     */
-    private function prepareHtmlForDomConversion($html)
-    {
-        $htmlWithSelfClosingSlashes = $this->ensurePhpUnrecognizedSelfClosingTagsAreXml($html);
-        $htmlWithDocumentType = $this->ensureDocumentType($htmlWithSelfClosingSlashes);
-
-        return $this->addContentTypeMetaTag($htmlWithDocumentType);
     }
 
     /**
@@ -353,9 +201,10 @@ class CssInliner
 
         $excludedNodes = $this->getNodesToExclude($xPath);
         $cssRules = $this->parseCssRules($combinedCss);
+        $cssSelectorConverter = $this->getCssSelectorConverter();
         foreach ($cssRules['inlineable'] as $cssRule) {
             try {
-                $nodesMatchingCssSelectors = $xPath->query($this->cssSelectorConverter->toXPath($cssRule['selector']));
+                $nodesMatchingCssSelectors = $xPath->query($cssSelectorConverter->toXPath($cssRule['selector']));
             } catch (SyntaxErrorException $e) {
                 if ($this->debug) {
                     throw $e;
@@ -583,9 +432,9 @@ class CssInliner
     /**
      * Disables the removal of elements with `display: none` properties.
      *
-     * @deprecated will be removed in Emogrifier 3.0
-     *
      * @return void
+     *
+     * @deprecated will be removed in Emogrifier 3.0
      */
     public function disableInvisibleNodeRemoval()
     {
@@ -971,7 +820,7 @@ class CssInliner
     private function existsMatchForCssSelector(\DOMXPath $xPath, $cssSelector)
     {
         try {
-            $nodesMatchingSelector = $xPath->query($this->cssSelectorConverter->toXPath($cssSelector));
+            $nodesMatchingSelector = $xPath->query($this->getCssSelectorConverter()->toXPath($cssSelector));
         } catch (SyntaxErrorException $e) {
             if ($this->debug) {
                 throw $e;
@@ -1027,21 +876,6 @@ class CssInliner
 
         $headElement = $this->getHeadElement();
         $headElement->appendChild($styleElement);
-    }
-
-    /**
-     * Checks that $this->domDocument has a BODY element and adds it if it is missing.
-     *
-     * @return void
-     */
-    private function ensureExistenceOfBodyElement()
-    {
-        if ($this->domDocument->getElementsByTagName('body')->item(0) !== null) {
-            return;
-        }
-
-        $htmlElement = $this->domDocument->getElementsByTagName('html')->item(0);
-        $htmlElement->appendChild($this->domDocument->createElement('body'));
     }
 
     /**
@@ -1144,76 +978,6 @@ class CssInliner
     }
 
     /**
-     * Makes sure that the passed HTML has a document type.
-     *
-     * @param string $html
-     *
-     * @return string HTML with document type
-     */
-    private function ensureDocumentType($html)
-    {
-        $hasDocumentType = \stripos($html, '<!DOCTYPE') !== false;
-        if ($hasDocumentType) {
-            return $html;
-        }
-
-        return static::DEFAULT_DOCUMENT_TYPE . $html;
-    }
-
-    /**
-     * Adds a Content-Type meta tag for the charset.
-     *
-     * This method also ensures that there is a HEAD element.
-     *
-     * @param string $html
-     *
-     * @return string the HTML with the meta tag added
-     */
-    private function addContentTypeMetaTag($html)
-    {
-        $hasContentTypeMetaTag = \stripos($html, 'Content-Type') !== false;
-        if ($hasContentTypeMetaTag) {
-            return $html;
-        }
-
-        // We are trying to insert the meta tag to the right spot in the DOM.
-        // If we just prepended it to the HTML, we would lose attributes set to the HTML tag.
-        $hasHeadTag = \stripos($html, '<head') !== false;
-        $hasHtmlTag = \stripos($html, '<html') !== false;
-
-        if ($hasHeadTag) {
-            $reworkedHtml = \preg_replace('/<head(.*?)>/i', '<head$1>' . static::CONTENT_TYPE_META_TAG, $html);
-        } elseif ($hasHtmlTag) {
-            $reworkedHtml = \preg_replace(
-                '/<html(.*?)>/i',
-                '<html$1><head>' . static::CONTENT_TYPE_META_TAG . '</head>',
-                $html
-            );
-        } else {
-            $reworkedHtml = static::CONTENT_TYPE_META_TAG . $html;
-        }
-
-        return $reworkedHtml;
-    }
-
-    /**
-     * Makes sure that any self-closing tags not recognized as such by PHP's DOMDocument implementation have a
-     * self-closing slash.
-     *
-     * @param string $html
-     *
-     * @return string HTML with problematic tags converted.
-     */
-    private function ensurePhpUnrecognizedSelfClosingTagsAreXml($html)
-    {
-        return \preg_replace(
-            '%<' . static::PHP_UNRECOGNIZED_VOID_TAGNAME_MATCHER . '\\b[^>]*+(?<!/)(?=>)%',
-            '$0/',
-            $html
-        );
-    }
-
-    /**
      * @param string[] $a
      * @param string[] $b
      *
@@ -1312,7 +1076,7 @@ class CssInliner
         $excludedNodes = [];
         foreach (\array_keys($this->excludedSelectors) as $selectorToExclude) {
             try {
-                $matchingNodes = $xPath->query($this->cssSelectorConverter->toXPath($selectorToExclude));
+                $matchingNodes = $xPath->query($this->getCssSelectorConverter()->toXPath($selectorToExclude));
             } catch (SyntaxErrorException $e) {
                 if ($this->debug) {
                     throw $e;
