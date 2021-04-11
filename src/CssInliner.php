39e48192bd16613801f4a2e9a666499105d38bf5
Oliver Klee <github@oliverklee.de>
2021-04-11 19:34:16 +0200

    Consolidate a nested condition

--- src/CssInliner.php
+++ src/CssInliner.php
@@ -421,11 +421,9 @@ class CssInliner extends AbstractHtmlProcessor
         // in order to not overwrite existing style attributes in the HTML, we
         // have to save the original HTML styles
         $nodePath = $node->getNodePath();
-        if (\is_string($nodePath)) {
-            if (!isset($this->styleAttributesForNodes[$nodePath])) {
-                $this->styleAttributesForNodes[$nodePath] = $this->parseCssDeclarationsBlock($normalizedOriginalStyle);
-                $this->visitedNodes[$nodePath] = $node;
-            }
+        if (\is_string($nodePath) && !isset($this->styleAttributesForNodes[$nodePath])) {
+            $this->styleAttributesForNodes[$nodePath] = $this->parseCssDeclarationsBlock($normalizedOriginalStyle);
+            $this->visitedNodes[$nodePath] = $node;
         }
 
         $node->setAttribute('style', $normalizedOriginalStyle);
