57c7d35ea2f07f139c07ec7993f5794eaf226006
JakeQZ <jake@qzdesign.co.uk>
2021-04-22 22:21:40 +0100

    [CLEANUP] Move CSS Constraint tests to their TestCase (#1040)
    This means these `TestCase`s now cover their corresponding classes (as declared
    by `@covers`) and not also dependant classes, which have their own `TestCase`s
    now fully covering their functionality.
    
    Also added CSS datasets for a needle found in a larger haystack, and a needle
    only partially found in a haystack.

--- tests/Unit/Support/Constraint/StringContainsCssCountTest.php
+++ tests/Unit/Support/Constraint/StringContainsCssCountTest.php
@@ -5,19 +5,164 @@ declare(strict_types=1);
 namespace Pelago\Emogrifier\Tests\Unit\Support\Constraint;
 
 use Pelago\Emogrifier\Tests\Support\Constraint\StringContainsCssCount;
+use Pelago\Emogrifier\Tests\Support\Traits\CssDataProviders;
 use Pelago\Emogrifier\Tests\Support\Traits\TestStringConstraint;
 use PHPUnit\Framework\Constraint\Constraint;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Note that the majority of the functionality is tested indirectly via
- * {@see \Pelago\Emogrifier\Tests\Unit\Support\Traits\AssertCssTest}; those tests are not repeated here.
- *
  * @covers \Pelago\Emogrifier\Tests\Support\Constraint\StringContainsCss
  */
 final class StringContainsCssCountTest extends TestCase
 {
+    use CssDataProviders;
     use TestStringConstraint;
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideCssNeedleNotFoundInHaystack
+     */
+    public function matchesHaystackNotContainingNeedleWithZeroCount(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(0, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideEquivalentCss
+     * @dataProvider provideEquivalentCssInStyleTags
+     * @dataProvider provideCssNeedleFoundInLargerHaystack
+     */
+    public function notMatchesHaystackContainingNeedleWithZeroCount(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(0, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideEquivalentCss
+     * @dataProvider provideEquivalentCssInStyleTags
+     * @dataProvider provideCssNeedleFoundInLargerHaystack
+     */
+    public function matchesHaystackContainingExactlyOneNeedleWithCountOfOne(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(1, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideCssNeedleNotFoundInHaystack
+     */
+    public function notMatchesHaystackNotContainingNeedleWithCountOfOne(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(1, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideEquivalentCss
+     * @dataProvider provideEquivalentCssInStyleTags
+     * @dataProvider provideCssNeedleFoundInLargerHaystack
+     */
+    public function notMatchesHaystackContainingNeedleTwiceWithCountOfOne(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(1, $needle);
+
+        $result = $subject->evaluate($haystack . $haystack, '', true);
+
+        self::assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideEquivalentCss
+     * @dataProvider provideEquivalentCssInStyleTags
+     * @dataProvider provideCssNeedleFoundInLargerHaystack
+     */
+    public function matchesHaystackContainingNeedleTwiceWithCountOfTwo(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(2, $needle);
+
+        $result = $subject->evaluate($haystack . $haystack, '', true);
+
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideCssNeedleNotFoundInHaystack
+     */
+    public function notMatchesHaystackNotContainingNeedleWithCountOfTwo(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(2, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @dataProvider provideEquivalentCss
+     * @dataProvider provideEquivalentCssInStyleTags
+     * @dataProvider provideCssNeedleFoundInLargerHaystack
+     */
+    public function notMatchesHaystackContainingExactlyOneNeedleWithCountOfTwo(string $needle, string $haystack): void
+    {
+        $subject = new StringContainsCssCount(2, $needle);
+
+        $result = $subject->evaluate($haystack, '', true);
+
+        self::assertFalse($result);
+    }
 
     /**
      * @return Constraint
