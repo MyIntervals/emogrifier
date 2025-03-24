51c623944c1d32e074ac68822edde4f7f0126024
Oliver Klee <github@oliverklee.de>
2025-03-24 17:31:51 +0100

    [CLEANUP] Make a type annotation in a test more specific (#1398)

--- tests/Unit/Css/StyleRuleTest.php
+++ tests/Unit/Css/StyleRuleTest.php
@@ -65,7 +65,10 @@ final class StyleRuleTest extends TestCase
     }
 
     /**
-     * @return array<string, array{0: array<int, array{property: string, value: string}>, 1: string}>
+     * @return array<string, array{
+     *             0: list<array{property: non-empty-string, value: non-empty-string}>,
+     *             1: string,
+     *         }>
      */
     public function provideDeclarations(): array
     {
@@ -82,7 +85,7 @@ final class StyleRuleTest extends TestCase
     /**
      * @test
      *
-     * @param array<int, array{property: string, value: string}> $declarations
+     * @param list<array{property: non-empty-string, value: non-empty-string}> $declarations
      * @param string $expected
      *
      * @dataProvider provideDeclarations
