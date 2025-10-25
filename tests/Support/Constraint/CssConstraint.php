7acc942803753ec91d200ac67ecdd2c3cabdd90e
Oliver Klee <github@oliverklee.de>
2025-10-25 15:29:47 +0200

    [TASK] Use safe preg functions in `CssConstraint` (#1483)
    Part of #919

--- tests/Support/Constraint/CssConstraint.php
+++ tests/Support/Constraint/CssConstraint.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Pelago\Emogrifier\Tests\Support\Constraint;
 
-use Pelago\Emogrifier\Utilities\Preg;
 use PHPUnit\Framework\Constraint\Constraint;
 
 use function Safe\preg_match;
 use function Safe\preg_replace;
+use function Safe\preg_replace_callback;
 
 /**
  * Provides a method for use in constraints making assertions about CSS content where whitespace may vary.
@@ -167,8 +167,17 @@ abstract class CssConstraint extends Constraint
      */
     protected static function getCssRegularExpressionMatcher(string $css): string
     {
-        $callback = \Closure::fromCallable([self::class, 'getCssRegularExpressionReplacement']);
-        $matcher = (new Preg())->replaceCallback(self::CSS_REGULAR_EXPRESSION_PATTERN, $callback, $css);
+        $callback = static function (array $matches): string {
+            /** @var array<int<0, max>, string> $matches */
+            return self::getCssRegularExpressionReplacement($matches);
+        };
+        // The safe version is only available in "thecodingmachine/safe" for PHP >= 8.1.
+        if (\function_exists('Safe\\preg_replace_callback')) {
+            $matcher = preg_replace_callback(self::CSS_REGULAR_EXPRESSION_PATTERN, $callback, $css);
+        } else {
+            $matcher = \preg_replace_callback(self::CSS_REGULAR_EXPRESSION_PATTERN, $callback, $css);
+        }
+        \assert(\is_string($matcher));
 
         return self::getCssMatcherAllowingOptionalTrailingSemicolon($matcher, $css);
     }
@@ -188,7 +197,13 @@ abstract class CssConstraint extends Constraint
         };
         foreach (self::CSS_REGULAR_EXPRESSION_REPLACEMENTS as $index => $replacement) {
             if (($matches[$index] ?? '') !== '') {
-                $regularExpressionEquivalent = (new Preg())->replaceCallback($pattern, $callback, $replacement);
+                // The safe version is only available in "thecodingmachine/safe" for PHP >= 8.1.
+                if (\function_exists('Safe\\preg_replace_callback')) {
+                    $regularExpressionEquivalent = preg_replace_callback($pattern, $callback, $replacement);
+                } else {
+                    $regularExpressionEquivalent = \preg_replace_callback($pattern, $callback, $replacement);
+                }
+                \assert(\is_string($regularExpressionEquivalent));
                 break;
             }
         }
